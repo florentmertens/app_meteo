540df67a061a2d1d3f69ee2be2b932addc74afd8
Florent <mertens.florent00@gmail.com>
2023-08-15 00:03:39 +0200

    15/08/2023

--- main.js
+++ main.js
@@ -45,19 +45,51 @@ inputSearch.addEventListener('keyup', function(event) {
 });
 
 function searchLatLon(city) {
-  console.log('ville = '+city);
   const apiKey = '8c58e7603a9d6defcdf272a660fa0c15';
   
   fetch('http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+apiKey)
     .then(response => response.json())
     .then(data => {
-      console.log(data[0]);
       const latitude = data[0].lat;
       const longitude = data[0].lon;
-      console.log('lat : '+latitude);
-      console.log('lon : '+longitude);
+      searchCity(latitude, longitude);
     })
     .catch(error => {
       console.error('erreur = '+error);
     });
+}
+
+function searchCity(latitude, longitude) {
+  const apiKey = '8c58e7603a9d6defcdf272a660fa0c15';
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+apiKey+'&units=metric')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const ville = data.name;
+    const temp = data.main.temp;
+    const tempMax = data.main.temp_max;
+    const tempMin = data.main.temp_min;
+    console.log("ville = " + ville);
+    console.log("temp = " + temp);
+    console.log("tempMax = " + tempMax);
+    console.log("tempMin = " + tempMin);
+    if (data.weather[0].id >= 200 && data.weather[0].id <= 232) {
+      console.log("tonner");
+    } else if (data.weather[0].id >= 300 && data.weather[0].id <= 321 && data.weather[0].id >= 500 && data.weather[0].id <= 531) {
+      console.log("pluie");
+    } else if (data.weather[0].id >= 600 && data.weather[0].id <= 622) {
+      console.log("neige");
+    } else if (data.weather[0].id >= 700 && data.weather[0].id <= 781) {
+      console.log("brume");
+    } else if (data.weather[0].id >= 801 && data.weather[0].id <= 804) {
+      console.log("nuage");
+    }  else if (data.weather[0].id == 800) {
+      console.log("soleil");
+    }
+    console.log("sunrise = "+new Date(data.sys.sunrise*1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));   
+    console.log("sunset = "+new Date(data.sys.sunset*1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));   
+    console.log("wind = "+data.wind.speed*3.6);   
+    console.log("humidity = "+data.main.humidity);   
+  })
 }
